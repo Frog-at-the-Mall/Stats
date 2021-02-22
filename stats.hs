2fc4577f07a9ef362e36fd797c6231940135a215
Matthew Frye <mattb.frye@gmail.com>
2021-02-21 23:04:37 -0500

    initial commit

---
+++ stats.hs
@@ -0,0 +1,27 @@
+-- janky stats formulas for and by mattFrye 022021
+
+import Data.List
+import Data.Char
+import Data.Map (insertWith', empty, filter, elems, keys)
+
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+  let smallerSorted = quicksort [a | a <- xs, a <= x]
+      biggerSorted = quicksort [a | a <- xs, a > x]
+  in  smallerSorted ++ [x] ++ biggerSorted
+
+mean xs = realToFrac (sum xs) / genericLength xs
+
+median xs = let a = quicksort(xs)in a !! ceiling(genericLength xs / 2)
+
+--i didnt write this one
+--mode xs
+
+midrange xs = (minimum xs + maximum xs)/2
+
+--measures of dispersion
+
+range xs = let a = quicksort xs in maximum a - minimum a
+
